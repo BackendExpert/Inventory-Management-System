a0fa7d7a59f6cf0783482fcea631fa171a90b25b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-22 18:10:23 +0530

    Updaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include("lib/layouts/header.php"); ?>
-
+<link rel="stylesheet" href="css/app.min.css">
 <section class="home-first">
     <div class="site-container">
         <h1 class="jk-display-3"><b>Welcome to Our Site</b></h1>
