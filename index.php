f035845d60ee429aa19cc5c5e32d2d9582df4352
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-22 18:31:02 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <div class="site-container">
         <div class="home-content">
             <h1 class="jk-display-3"><b>Welcome to Our Site</b></h1>
+            <a href=""><button class="jkbtn jkbtn-blue-outline login-btn">Login</button></a>
         </div>
     </div>
 </section>
