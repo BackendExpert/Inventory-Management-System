250852d878ac3d3e41c84e6f76cb0787e5dde26b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-22 18:21:13 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <link rel="stylesheet" href="css/app.min.css">
 <section class="home-first">
     <div class="site-container">
-        <h1 class="jk-display-3"><b>Welcome to Our Site</b></h1>
+        <div class="home-content">
+            <h1 class="jk-display-3"><b>Welcome to Our Site</b></h1>
+        </div>
     </div>
 </section>
 
